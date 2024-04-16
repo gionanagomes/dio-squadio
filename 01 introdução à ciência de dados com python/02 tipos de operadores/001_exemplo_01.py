41c2f01e97cd61394a04bed10ab1f7fb351366d8
Giovanna <giovannagomes5476@gmail.com>
2024-04-16 18:47:37 -0300

    atualização do conteúdo

---
+++ 01 introdução à ciência de dados com python/02 tipos de operadores/001_exemplo_01.py
@@ -0,0 +1,20 @@
+# adição
+print(1 + 1)
+
+#subtração
+print(10 - 2)
+
+# multiplicação
+print(4 * 3)
+
+# divisão
+print(12 / 3)
+
+# divisão inteira
+print(12 // 2)
+
+# módulo
+print(10 % 3)
+
+# exponenciação
+print(2 ** 3)
---
+++ 01 introdução à ciência de dados com python/02 tipos de operadores/001_exemplo_02.py
@@ -0,0 +1,6 @@
+# precedência dos operadores
+print(10 - 5 * 2)
+print((10 - 5) * 2)
+print(10 ** 2 * 2)
+print(10 ** (2 * 2))
+print(10 / 2 * 4)
