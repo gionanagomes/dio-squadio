da47104df3a5396cce9e892efeedb869434d46d7
Giovanna <giovannagomes5476@gmail.com>
2024-04-15 21:30:01 -0300

    atualização do conteúdo

---
+++ 01 introdução à ciência de dados com python/01 conhecendo a linguagem de programação python/001_tipos_de_dados.py
@@ -0,0 +1,12 @@
+# inteiro
+print(11 + 10 + 1000)
+
+# float
+print(1.5 + 1 + 0.5)
+
+# booleano
+print(True)
+print(False)
+
+# string
+print("Python")
---
+++ 01 introdução à ciência de dados com python/01 conhecendo a linguagem de programação python/002_variaveis_e_constantes.py
@@ -0,0 +1,11 @@
+nome = 'Guilherme'
+idade = 28
+
+nome = 'Giovanna'
+
+print(nome, idade)
+
+limite_saque_diario = 1000
+
+ESTADOS = ["SP", "RJ", "SC", "RS"]
+print(ESTADOS)
